17a5dc811ca21f59e437d330ae9204f98ae45b6f
afsumeem <afsumeem@gmail.com>
2022-05-19 00:10:10 +0600

    product page done

--- src/pages/Products/Product/Product.js
+++ src/pages/Products/Product/Product.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
-const Product = () => {
+const Product = ({ product }) => {
+    const { name, desc, picture, _id } = product;
+
+    const imgStyle = {
+        height: '220px'
+    };
+
     return (
-        <div>
+        <div className="mt-5">
+
+            <Col>
+                <Card className=" m-3">
+                    <Card.Img className="w-100" variant="top" src={picture} style={imgStyle} />
+                    <Card.Body className="p-2">
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text className="text-start">
+                            {desc.slice(0, 150)}
+                        </Card.Text >
+                    </Card.Body>
+                    <Card.Footer>
+                        <NavLink to={`/products/${_id}`} className="text-decoration-none fs-5">
+                            View Details
+                        </NavLink>
+                    </Card.Footer>
+
+                </Card>
+            </Col>
+
 
         </div>
     );
--- src/pages/Products/Products/Products.js
+++ src/pages/Products/Products/Products.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Header from '../../Shared/Header/Header';
+import Product from '../Product/Product';
 
 const Products = () => {
-    return (
-        <div>
+    const [products, setProducts] = useState([]);
 
-        </div>
+
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data.slice(0, 6)))
+    }, []);
+
+
+    return (
+        <>
+            <Header />
+            <Container>
+
+                <h2 className="pt-5 pb-3 heading">Discover the Tech Innovations</h2>
+
+                <Row xs={1} md={3} sm={2} className="g-5">
+                    {
+                        products.map(product => <Product
+                            key={product._id}
+                            product={product}
+                        >
+                        </Product>)
+                    }
+                </Row>
+            </Container>
+        </>
     );
 };
 
