51becef389e01770815b2c4866b98353acf784a5
afsumeem <afsumeem@gmail.com>
2022-05-18 11:43:44 +0600

    firebase setup

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,20 @@
+import React, { createContext } from 'react';
+
+import useFirebase from '../hooks/useFirebase';
+
+
+//creating context
+export const AuthContext = createContext();
+
+
+const AuthProvider = ({ children }) => {
+    const allContext = useFirebase();
+    return (
+
+        <AuthContext.Provider value={allContext}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
